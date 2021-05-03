7e10530b70adba4baf449b451cb96be33b9a0d06
gabrielesala <gabriele.sala@studenti.unitn.it>
2021-05-03 03:20:50 +0200

    solved svg description issues

--- library/mds.cpp
+++ library/mds.cpp
@@ -1,13 +1,15 @@
-using namespace std;
 #include <iostream>
+#include <string.h>
+#include <cmath>
 #include <fstream>
-#include <math.h>
+#include <streambuf>
 #include <string>
-#include "../library/mds.h"
+#include <sstream>
+#include "mds.h"
 
 int mds_inspect(mds* mds){  // funzione che controlla i parametri
     int ins;
-    double max_pl = 1/2 * Lb;  // definisco la lunghezza massima del pistone
+    double max_pl = 1/2 * (mds->Lb);  // definisco la lunghezza massima del pistone
     if (mds -> Dm > 0 && 
         mds -> Lm > 0 &&
         mds -> Lp > 0 &&
@@ -15,16 +17,17 @@ int mds_inspect(mds* mds){  // funzione che controlla i parametri
         mds -> Hp > 0 &&
         mds -> alfa > 0)
     {ins = 0;}
-    if (mds -> Lm sin(alfa) > Lb){  // imposto il primo constrain
-        cout << "Error 101! The baricenter's position value has to be lower than the length of the chassis, so < than " << Lb << endl;
+    if ((mds -> Lm) * sin((mds->alfa)) > (mds->Lb))
+    {  // imposto il primo constrain
+        cout << "Error 101! The baricenter's position value has to be lower than the length of the chassis, so < than " << (mds->Lb) << endl;
     ins = 101;
     }
 
-    if (mds-> Lm cos(alfa) >= 1/2 Hp){ // imposto il secondo constrain
+    if ((mds-> Lm) * cos((mds->alfa)) >= 1/2 * (mds->Hp)){ // imposto il secondo constrain
         cout << "Error 102! The vertical length of the cranck " << endl;}
     ins = 102;
 
-    if (mds-> Lp <= max_pl){  // imposto l'ultimo constrain
+    if ((mds-> Lp) <= max_pl){  // imposto l'ultimo constrain
         cout << "Error 103! The length of the piston has to be lower than 1/2 of the chassis length, so < than " << max_pl << endl;}
     ins = 103;
 
@@ -37,37 +40,37 @@ string mds_to_svg(mds* mds){   // definizione file svg (ERROR, da sistemare)
     string defxml;
     defxml =  "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"800\" height=\"600\">\n\n";
     string rect1;
-    rect1 = "<rect width=\""750"": // blocked values
-    rect1 += "height="150"";
-    rect1 += "transform="translate(50,100)"";
-    rect1 += "style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,255)\"/> \n";
+    rect1 = " <rect width=\"750\ ";
+    rect1 += " height=\" 150 ";
+    rect1 += " transform=\"translate("50","100") ";
+    rect1 += "style=\"fill:rgb(0,0,255)stroke-width:3 stroke:rgb(0,0,255)\"/> \n";
     
     
     string cranck;
-    cranck = "<rect x="+ to_string(50 + Lm/2 cos(alfa+ 3/2 Pi))"";
-    cranck += "y="+ to_string(100 + Lm/2 Sin(alfa+ 3/2 Pi))"";
-    cranck += "width="+ to_string(Lm)""; 
-    cranck += "height=\"+ to_string(Lp";
-    cranck += "transform="rotate(alfa - 90 Pi)" /> \n";
+    cranck = "<rect x=\""+ to_string(50 + (mds->Lm)/2 * cos((mds->alfa)+ 3/2 * 3.14))+"\"";
+    cranck += "y=\""+ to_string(100 + (mds->Lm)/2 * sin((mds->alfa)+ 3/2 * 3.14))+"\"";
+    cranck += "width=\""+ to_string((mds->Lm)); 
+    cranck += "height=\""+ to_string((mds->Lp));
+    cranck += "transform=\"rotate("+ to_string((mds->alfa) - 90 * 3.14)+ "/> \n";
 
     string piston;
-    piston = "<rect x="+ to_string(50 + Lm cos(alfa+ 3/2 Pi))"";
-    piston += "y="+ to_string(100 + Lm/2 sin(alfa+ 3/2 Pi))"";
-    piston += "width="+ to_string(Lp)";
-    piston += "height="+ to_string(Hp)/>\n";
+    piston = "<rect x=\""+ to_string(50 + (mds->Lm) * cos((mds->alfa)+ 3/2 * Pi))+"\"";
+    piston += "y=\""+ to_string(100 + (mds->Lm)/2 * sin((mds->alfa)+ 3/2 * Pi))+"\"";
+    piston += "width=\""+ to_string((mds->Lp))+"\"";
+    piston += "height=\""+ to_string((mds->Hp)) " /> \n";
 
 
     string circle2;
-    circle1 = "<circle cx="50 + Lm cos(alfa+ 3/2 Pi)" cy="100 + Lm/2 sin(alfa+ 3/2 Pi)" r="30" stroke="black" stroke-width="3" fill="red" /> \n";
+    circle2 = "<circle cx=\"" + to_string(50 + (mds->Lm) * cos((mds->alfa) + 3/2 * Pi)) + "\"+ cy="+to_string(100 + (mds->Lm)/2 sin((mds->alfa)+ 3/2 Pi))" r="30" stroke=\"black\" stroke-width="3" fill=\"red\" /> \n";
 
     string rect2;
     rect2 = "<rect width="750"";
     rect2 += "height="150"";
-    rect2 += "transform="translate(50, + to_string =(100 + Lm sin(alfa) + 2 Hp))"";
-    rect2 += "style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,255)\" /> \n";
+    rect2 += "transform=\"translate("50 +"," + to_string =(100 + (mds->Lm) * sin((mds->alfa)) + 2 * (mds->Hp)))"";
+    rect2 += "style=\"fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,255)\" /> \n";
 
     string circle1;
-    circle1 = "<circle cx="50" cy="100" r="30" stroke="black" stroke-width="3" fill="red" /> \n";
+    circle1 = "<circle cx="50" cy="100" r="30" stroke=\"black\" stroke-width="3" fill=\"red\" /> \n";
 
     string mds_to_svg = rect1 + cranck + piston + circle2 + rect2 + circle1;
     return mds_to_svg;
