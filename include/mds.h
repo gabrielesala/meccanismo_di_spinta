00e0fce940a1b8e469aefb69a91257c82a6a9569
gabrielesala <gabriele.sala@studenti.unitn.it>
2021-05-06 06:25:19 +0200

    Changed structure of the project, added include directory

---
+++ include/mds.h
@@ -0,0 +1,34 @@
+  
+#ifndef MDS_H
+#define MDS_H
+
+#include <iostream>
+#include <string>
+
+struct mds
+{
+    double Dm;
+    double Lm;
+    double Lp;
+    double Lb;
+    double Hp;
+	double alfa;
+
+    mds(double _Dm, double _Lm, double _Lp, double _Lb, double _Hp, double _alfa){ // definisco i parametri variabili
+        Dm = 0;
+		Lm = 0;
+        Lp = 0;
+        Lb = 0;
+        Hp = 0;
+	    alfa = 0;
+    }
+};
+
+void mg_filew(std::string mds_to_svg); 
+std::string mds_to_svg(mds* mds);  // crea la stringa
+int mds_inspect(mds* mds);    //controlla i vincoli
+void init_mds(mds* mds);    //crea un nuovo meccanismo di spinta inizializzando nuove variabili
+void choose_option(int opt); //permette di scegliere la modalità 
+
+
+#endif
