89ab397f3381e36282d74c52fa9032eedf83dccb
gabrielesala <gabriele.sala@studenti.unitn.it>
2021-05-06 06:28:34 +0200

    Defined main.cpp, added choose_option function from mds.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 using namespace std;
 
-
 #include <iostream>
 #include <string>
-#include "../meccanismo_di_spinta/library/mds.h"
+#include <cmath>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+#include "../meccanismo_di_spinta/include/mds.h"
 
 
 int main(){
-    
-    // da definire
-    
+    //using switch case 
+    int opt;
+    cin >> opt;
+    choose_option(opt);
 return 0;
 }
